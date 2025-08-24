01763c825df1a8f6321b5ca33eccd61fb106e34c
KaileyLiou <kaileyliou09@gmail.com>
2025-08-24 19:12:32 -0400

    changed colors

--- script.js
+++ script.js
@@ -41,7 +41,6 @@ setInterval(() => {
     angle: 90,
     spread: 45,
     origin: { x: Math.random(), y: 0 },
-    // colors: ['#ff69b4', '#ffd700', '#87ceeb', '#ffb6c1']
     colors: ['#c71585', '#b8860b', '#4682b4', '#dc143c'],
     gravity: 1,
     ticks: 200

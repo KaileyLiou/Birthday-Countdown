e5169ad592c889d7f76a4c56428fcaf1b61b0bdf
KaileyLiou <kaileyliou09@gmail.com>
2025-08-24 15:08:52 -0400

    added confetti

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function startCountdown() {
-    
+
 }
+
+setInterval(() => {
+  confetti({
+    particleCount: 2,
+    angle: 90,
+    spread: 45,
+    origin: { x: Math.random(), y: 0 },
+    colors: ['#ff69b4', '#ffd700', '#87ceeb', '#ffb6c1']
+  });
+}, 200);
